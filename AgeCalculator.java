7ad55cfe224308e9594d732eae7fcb0394a416ca
Thomas <thomascastanop@gmail.com>
2025-02-28 10:45:57 -0500

    Exam

--- AgeCalculator.java
+++ AgeCalculator.java
@@ -5,3 +5,16 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+public class AgeCalculator{ 
+   /* private int currentyear = 2025; 
+    public int ageCalculator(int yearOfBirth){
+        int age = 2025 - yearOfBirth; 
+        if (age >= 16){
+            System.out.prinln("Eligible to drive"); 
+        } else{
+            System.out.println("Must wait" + 16 - age +"years to drive"); 
+        }
+        return AgeCalculator;
+    }
+    */
+}
--- Salary.java
+++ Salary.java
@@ -6,6 +6,8 @@
  * @version (a version number or a date)
  */
 public class Salary {
+    private int hours; 
+    private int wage; 
   
   /*  public netPay(int hours, int wage){
         int salary = hours * wage; 
@@ -15,7 +17,15 @@ public class Salary {
         
      
 }*/ 
+public double netPay(int hours, int wage){
+    var salary = hours * wage;
+    var deductions = salary * 0.3; 
+    var calculateNetPay = salary - deductions; 
+    System.out.println("Your net pay is " + calculateNetPay); 
+    return calculateNetPay; 
 }
+}
+
 
 
 
