12bb5c35c9082e3b014692ed26a482ab15c86baa
Thomas <thomascastanop@gmail.com>
2025-02-28 10:32:43 -0500

    The EXACT code I wrote

--- Salary.java
+++ Salary.java
@@ -5,29 +5,17 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Salary
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Salary
-     */
-    public Salary()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+public class Salary {
+  
+  /*  public netPay(int hours, int wage){
+        int salary = hours * wage; 
+        double calculateNetPay = salary * 0.3; 
+       
+        System.out.println("Your net pay is  "+ calculateNetPay); 
+        
+     
+}*/ 
 }
+
+
+
